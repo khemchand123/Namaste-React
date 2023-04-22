d5ed8d834cb06d735e7f7860bddc37fe52302aad
Khem Chand <Khem.Chand@cognizant.com>
2023-04-22 18:10:52 +0530

    third episode

--- app.js
+++ app.js
@@ -1,40 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-/**
- * 
- * 
- *   <div id="parent">
- *       <div id="child">
- *         <h1>h1 content for heading!<h1>
- *       </div>
- *   </div>
- * 
- * 
- * 
- * 
- */
-const parent = React.createElement(
-    "div", { id: "parent" },
-    [
-        React.createElement("div", { id: "child" },
-            [React.createElement("h1", {}, "h1 content for heading !"),
-            React.createElement("h2", {}, "h2 content for heading !")
-            ]),
-            React.createElement("div", { id: "child" },
-            [React.createElement("h1", {}, "h1 content for heading !"),
-            React.createElement("h2", {}, "h2 content for heading !")
-            ])
-    ]
-);
 
-const heading = React.createElement("h1",
-    {
-        id: "heading",
-        xyz: "key001"
-    },
-    "Hello World from React!");
+const Heading = () => (
+    <h1>Namaste React from JSX</h1>
+)
+
+//React Component
+const HeadingComponent = () => (
+    <div id="heading" className="heading">
+        <Heading />
+        <h1 tabIndex="1">Namaste React Component!</h1>
+    </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
-
+root.render(<HeadingComponent/>);
